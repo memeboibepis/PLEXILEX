ce7cb047098ebf75396470a36f874737013e39e1
willo <153154914+willowastaken@users.noreply.github.com>
2024-10-06 23:19:09 -0500

    Update function.js

--- js/shortcuts/function.js
+++ js/shortcuts/function.js
@@ -2,4 +2,14 @@ function openChromeTab() {
     document.getElementById("chrome").click();  
 }
 
+function openPVZTab() {
+    document.getElementById("pvz").click();  
+}
+
+function openGeometryDashTab() {
+    document.getElementById("geometrydash").click();  
+}
+
 document.getElementById("shortcutchrome").addEventListener("click", openChromeTab);
+document.getElementById("shortcutpvz").addEventListener("click", openPVZTab);
+document.getElementById("shortcutgeometrydash").addEventListener("click", openGeometryDashTab);
