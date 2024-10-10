f5331d21b81f7149db8d6918e89956ae898f154f
willo <153154914+willowastaken@users.noreply.github.com>
2024-10-09 23:21:18 -0500

    Update drag.js

--- js/drag.js
+++ js/drag.js
@@ -1,1 +1,30 @@
+const selectionBox = document.getElementById('selectionBox');
+let startX, startY;
 
+document.addEventListener('mousedown', (e) => {
+    startX = e.pageX;
+    startY = e.pageY;
+    
+    selectionBox.style.width = '0px';
+    selectionBox.style.height = '0px';
+    selectionBox.style.left = `${startX}px`;
+    selectionBox.style.top = `${startY}px`;
+    selectionBox.style.display = 'block';
+});
+
+document.addEventListener('mousemove', (e) => {
+    if (e.buttons === 1) { 
+        const width = Math.abs(e.pageX - startX);
+        const height = Math.abs(e.pageY - startY);
+
+        selectionBox.style.width = `${width}px`;
+        selectionBox.style.height = `${height}px`;
+
+        selectionBox.style.left = `${Math.min(e.pageX, startX)}px`;
+        selectionBox.style.top = `${Math.min(e.pageY, startY)}px`;
+    }
+});
+
+document.addEventListener('mouseup', () => {
+    selectionBox.style.display = 'none';
+});
